a02d08e7ff91094aaa183abdf1cfcafda2bedfab
nickibyte <nicholas.klug@web.de>
2019-10-04 23:44:35 +0200

    Piece height now determined by type

--- js/tridchess.js
+++ js/tridchess.js
@@ -1,66 +1,3 @@
-
-/*	var board = [ [ [ null, 'a1^2', null,null,null,null ],
-	           	    [ null, 'a2^2', null,null,null,null ],
-	           	    [ null, null, null,'a3^4',null,null ],
-	           	    [ null, null, null,'a4^4',null,null ],
-	           	    [ null, 'a5^2', null,null,null,'a5^6' ],
-	           	    [ null, 'a6^2', null,null,null,'a6^6' ],
-	           	    [ null, null, null,'a7^4',null,null ],
-	           	    [ null, null, null,'a8^4',null,null ],
-	           	    [ null, null, null,null,null,'a9^6' ],
-	           	    [ null, null, null,null,null,'a10^6' ] ],
-	              [ [ null, 'b1^2', null,null,null,null ],
-	                [ 'b2^1', 'b2^2', null,null,null,null ],
-	                [ 'b3^1', null, null,'b3^4',null,null ],
-	                [ 'b4^1', null, 'b4^3','b4^4',null,null ],
-	                [ 'b5^1', 'b5^2', 'b5^3',null,null,'b5^6' ],
-	                [ null, 'b6^2', 'b6^3',null,'b6^5','b6^6' ],
-	                [ null, null, 'b7^3','b7^4','b7^5',null ],
-	                [ null, null, null,'b8^4','b8^5',null ],
-	                [ null, null, null,null,'b9^5','b9^6' ],
-	                [ null, null, null,null,null,'b10^6' ] ],
-	              [ [ null, null, null,null,null,null ],
-	                [ 'c2^1', null, null,null,null,null ],
-	                [ 'c3^1', null, null,null,null,null ],
-	                [ 'c4^1', null, 'c4^3',null,null,null ],
-	                [ 'c5^1', null, 'c5^3',null,null,null ],
-	                [ null, null, 'c6^3',null,'c6^5',null ],
-	                [ null, null, 'c7^3',null,'c7^5',null ],
-	                [ null, null, null,null,'c8^5',null ],
-	                [ null, null, null,null,'c9^5',null ],
-	                [ null, null, null,null,null,null ] ],
-	              [ [ null, null, null,null,null,null ],
-	                [ 'd2^1', null, null,null,null,null ],
-	                [ 'd3^1', null, null,null,null,null ],
-	                [ 'd4^1', null, 'd4^3',null,null,null ],
-	                [ 'd5^1', null, 'd5^3',null,null,null ],
-	                [ null, null, 'd6^3',null,'d6^5',null ],
-	                [ null, null, 'd7^3',null,'d7^5',null ],
-	                [ null, null, null,null,'d8^5',null ],
-	                [ null, null, null,null,'d9^5',null ],
-	                [ null, null, null,null,null,null ] ],
-	              [ [ null, 'e1^2', null,null,null,null ], 
-	                [ 'e2^1', 'e2^2', null,null,null,null ],
-	                [ 'e3^1', null, null,'e3^4',null,null ],
-	                [ 'e4^1', null, 'e4^3','e4^4',null,null ],
-	                [ 'e5^1', 'e5^2', 'e5^3',null,null,'e5^6' ],
-	                [ null, 'e6^2', 'e6^3',null,'e6^5','e6^6' ],
-	                [ null, null, 'e7^3','e7^4','e7^5',null ],
-	                [ null, null, null,'e8^4','e8^5',null ],
-	                [ null, null, null,null,'e9^5','e9^6' ],
-	                [ null, null, null,null,null,'e10^6' ] ],
-	              [ [ null, 'f1^2', null,null,null,null ],
-	                [ null, 'f2^2', null,null,null,null ],
-	                [ null, null, null,'f3^4',null,null ],
-	                [ null, null, null,'f4^4',null,null ],
-	                [ null, 'f5^2', null,null,null,'f5^6' ],
-	                [ null, 'f6^2', null,null,null,'f6^6' ],
-	                [ null, null, null,'f7^4',null,null ],
-	                [ null, null, null,'f8^4',null,null ],
-	                [ null, null, null,null,null,'f9^6' ],
-	                [ null, null, null,null,null,'f10^6' ] ] ]; */
-
-
 // ----------------------------------------------------------------
 // ----------------------------------------------------------------
 // Globals
@@ -135,47 +72,47 @@ var Tridchess = function(fen, notation) {
                           [ null, null, null, null, null, null ] ],
 
                         [ [ null, null, null, null, null, null ],
-                          [   ' ', null, null, null, null, null ],
-                          [   ' ', null, null, null, null, null ],
-                          [   ' ', null,   ' ', null, null, null ],
-                          [   ' ', null,   ' ', null, null, null ],
-                          [ null, null,   ' ', null,   ' ', null ],
-                          [ null, null,   ' ', null,   ' ', null ],
-                          [ null, null, null, null,   ' ', null ],
-                          [ null, null, null, null,   ' ', null ],
+                          [  ' ', null, null, null, null, null ],
+                          [  ' ', null, null, null, null, null ],
+                          [  ' ', null,  ' ', null, null, null ],
+                          [  ' ', null,  ' ', null, null, null ],
+                          [ null, null,  ' ', null,  ' ', null ],
+                          [ null, null,  ' ', null,  ' ', null ],
+                          [ null, null, null, null,  ' ', null ],
+                          [ null, null, null, null,  ' ', null ],
                           [ null, null, null, null, null, null ] ],
 
                         [ [ null, null, null, null, null, null ],
-                          [   ' ', null, null, null, null, null ],
-                          [   ' ', null, null, null, null, null ],
-                          [   ' ', null,   ' ', null, null, null ],
-                          [   ' ', null,   ' ', null, null, null ],
-                          [ null, null,   ' ', null,   ' ', null ],
-                          [ null, null,   ' ', null,   ' ', null ],
-                          [ null, null, null, null,   ' ', null ],
-                          [ null, null, null, null,   ' ', null ],
+                          [  ' ', null, null, null, null, null ],
+                          [  ' ', null, null, null, null, null ],
+                          [  ' ', null,  ' ', null, null, null ],
+                          [  ' ', null,  ' ', null, null, null ],
+                          [ null, null,  ' ', null,  ' ', null ],
+                          [ null, null,  ' ', null,  ' ', null ],
+                          [ null, null, null, null,  ' ', null ],
+                          [ null, null, null, null,  ' ', null ],
                           [ null, null, null, null, null, null ] ],
 
                         [ [ null, null, null, null, null, null ],
-                          [   ' ', null, null, null, null, null ],
-                          [   ' ', null, null, null, null, null ],
-                          [   ' ', null,   ' ', null, null, null ],
-                          [   ' ', null,   ' ', null, null, null ],
-                          [ null, null,   ' ', null,   ' ', null ],
-                          [ null, null,   ' ', null,   ' ', null ],
-                          [ null, null, null, null,   ' ', null ],
-                          [ null, null, null, null,   ' ', null ],
+                          [  ' ', null, null, null, null, null ],
+                          [  ' ', null, null, null, null, null ],
+                          [  ' ', null,  ' ', null, null, null ],
+                          [  ' ', null,  ' ', null, null, null ],
+                          [ null, null,  ' ', null,  ' ', null ],
+                          [ null, null,  ' ', null,  ' ', null ],
+                          [ null, null, null, null,  ' ', null ],
+                          [ null, null, null, null,  ' ', null ],
                           [ null, null, null, null, null, null ] ],
 
                         [ [ null, null, null, null, null, null ], 
-                          [   ' ', null, null, null, null, null ],
-                          [   ' ', null, null, null, null, null ],
-                          [   ' ', null,   ' ', null, null, null ],
-                          [   ' ', null,   ' ', null, null, null ],
-                          [ null, null,   ' ', null,   ' ', null ],
-                          [ null, null,   ' ', null,   ' ', null ],
-                          [ null, null, null, null,   ' ', null ],
-                          [ null, null, null, null,   ' ', null ],
+                          [  ' ', null, null, null, null, null ],
+                          [  ' ', null, null, null, null, null ],
+                          [  ' ', null,  ' ', null, null, null ],
+                          [  ' ', null,  ' ', null, null, null ],
+                          [ null, null,  ' ', null,  ' ', null ],
+                          [ null, null,  ' ', null,  ' ', null ],
+                          [ null, null, null, null,  ' ', null ],
+                          [ null, null, null, null,  ' ', null ],
                           [ null, null, null, null, null, null ] ],
 
                         [ [ null, null, null, null, null, null ],
@@ -217,7 +154,7 @@ var Tridchess = function(fen, notation) {
 	var board = MAIN_BOARDS;
 	var towers = [];
 	
-	loadFen(DEFAULT_STARTING_FEN);
+	loadFen(fen);
 
 	function addTowers(towers) {
 
@@ -245,11 +182,9 @@ var Tridchess = function(fen, notation) {
 		towers = [];
 
 		var fields = fen.split(' ');
-		console.log("Got fields from fen: " + fields);
 
 		// Get tower positions
 		var towerPos = fields[0].split('');
-		console.log("Got tower positions from fen: " + towerPos);
 
 		// Convert 12-base position to integer
 		for (var i = 0; i < towerPos.length; i++) {
@@ -257,7 +192,6 @@ var Tridchess = function(fen, notation) {
 			towerPos[i] = parseInt(towerPos[i], 13);
 
 		}
-		console.log("Converted tower positions to: " + towerPos)
 
 		// Set towers and add them to the static board
 		towers = towerPos;
@@ -289,10 +223,11 @@ var Tridchess = function(fen, notation) {
 			for (var r = 0; r < board[f].length; r++) {
 				for (var l = 0; l < board[f][r].length; l++) {
 
-					if (board[f][r][l] == ' ') {
+					if (board[f][r][l] === ' ') {
 
 						board[f][r][l] = position[i];
 						i++;
+
 					}
 
 				}
