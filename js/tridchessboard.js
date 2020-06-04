b2975bc1c6a182678e236f21d895f328aad708b9
nickibyte <nicholas.klug@web.de>
2020-06-04 18:52:02 +0200

    Added copyright and license notice according to FSF licensing instructions

--- js/tridchessboard.js
+++ js/tridchessboard.js
@@ -1,3 +1,22 @@
+// tridchessboard.js
+// https://github.com/nickibyte/tridchessboard.js
+//
+// Copyright (c) 2020 nickibyte <https://github.com/nickibyte>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+
 import * as THREE from './three.js/build/three.module.js';
 import { OrbitControls } from './three.js/examples/jsm/controls/OrbitControls.js';
 import { DragControls } from './three.js/examples/jsm/controls/DragControls.js';
